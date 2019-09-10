1865923b70961615a1f3484da93a0db4e1eabea7
leticiamrosa <leticiamonteiro.design@gmail.com>
2019-09-09 22:51:29 -0300

    challenge 27 done

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,4 +1,6 @@
-/*
+(function() {
+  "use strict";
+  /*
 Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
 métodos semelhantes aos que existem no array, mas que sirvam para os
 elementos do DOM selecionados.
@@ -19,3 +21,87 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+  // ?
+
+  // construtor
+  function DOM(elements) {
+    this.element = this.getDOMElements(elements); // todos os elmentos do DOM selecionados
+  }
+
+  // metodo para selecionar os elementos
+  DOM.prototype.getDOMElements = function getDOMElements(elements) {
+    return document.querySelectorAll(elements);
+  };
+
+  // metodos
+  // adiciona eventos
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, element => {
+      element.addEventListener(eventType, callback, false);
+    });
+  };
+
+  // remove eventos
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, element => {
+      element.removeEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  // forEarch
+  DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply(this.element, arguments);
+  };
+
+  // map
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+  };
+
+  // filter
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  // reduce
+  DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  // reduceRight
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  // every
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments);
+  };
+
+  // some
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments);
+  };
+
+  let $a = new DOM('[data-js="link"]');
+  let dataJs = $a.map(item => {
+    return item.getAttribute("data-js");
+  });
+  console.log(dataJs);
+
+  let dataJsReduce = $a.reduce((acc, item, index) => {
+    return `${acc} ${item.getAttribute("data-js")} ${index}`;
+  }, 0);
+  console.log(dataJsReduce);
+
+  DOM.prototype.isArray = function isArray(param) {
+    return Object.prototype.toString.call(param) === "[object Array]";
+  };
+
+  console.log(DOM.isArray([1, 2, 3]));
+})();
---
+++ notes/164-tecnicas ninjas/index.js
@@ -0,0 +1,8 @@
+(function() {
+  "use strict";
+
+  let arr = [1, 2, 3, 4, 5];
+  let arr2 = arr.map(item => item);
+
+  console.log(arr2.slice(2, 3));
+})();
